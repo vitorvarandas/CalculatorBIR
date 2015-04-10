8e7ab9f21282882e6d153af38daa1563d74579a6
Gustavo Knittel <gugakm22@gmail.com>
2015-04-10 16:46:35 -0300

    Creating the program to evaluate the tree.

---
+++ evaluate/evaluate.hpp
@@ -0,0 +1,53 @@
+#ifndef EVALUATE_INCLUDED
+#define EVALUATE_INCLUDED
+#include <string>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class evaluate
+{
+
+public:
+
+    void evaluatetree(node_type* prt)
+    {
+
+        if(IsOp(prt->data) && !IsOp(prt->right_child->data) && !IsOp(prt->left_child->data)){
+        double num;
+        double num1 = atof(right_child->prt.c_str());
+        double num2 = atof(left_child->prt.c_str());
+        if(op == '+')
+        {
+            num = num1 + num2;
+        }
+        else if (op == '-')
+        {
+            num = num1 - num2;
+        }
+        else if(op == '/')
+        {
+            num = num1 / num2;
+        }
+        else if (op == '*')
+        {
+            num = num1 * num2;
+        }
+
+        stringstream ss;
+        string result;
+        ss << num;
+        ss >> result;
+
+        cout<<"Resultado = "<< result <<endl;
+
+    }
+};
+
+
+
+
+
+
+
+#endif // EVALUATE_INCLUDED
