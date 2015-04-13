40298139f589b7f046a023a22d9b62fce247a55c
Gustavo Knittel <gugakm22@gmail.com>
2015-04-13 15:19:51 -0300

    versao 1 do evaluatec

--- evaluate/evaluate.hpp
+++ evaluate/evaluate.hpp
@@ -15,8 +15,8 @@ public:
 
         if(IsOp(prt->data) && !IsOp(prt->right_child->data) && !IsOp(prt->left_child->data)){
         double num;
-        double num1 = atof(right_child->prt.c_str());
-        double num2 = atof(left_child->prt.c_str());
+        double num1 = atof(ptr->right_child.c_str()->data.c_str());
+        double num2 = atof(ptr->left_child.c_str()->data.c_str());
         if(op == '+')
         {
             num = num1 + num2;
