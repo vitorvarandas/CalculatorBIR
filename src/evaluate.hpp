eee1e93907ff787e5132dd32d5f3afa80edf0524
Gustavo Knittel <gugakm22@gmail.com>
2015-04-15 10:38:27 -0300

    Versao 2 do evaluate

--- src/evaluate.hpp
+++ src/evaluate.hpp
@@ -1,47 +1,14 @@
 #ifndef EVALUATE_INCLUDED
 #define EVALUATE_INCLUDED
-#include <string>
-#include <iostream>
-#include <sstream>
-using namespace std;
+
 
 class evaluate
 {
-
 public:
 
-    void evaluatetree(node_type* prt)
-    {
+void evaluate(node_type* ptr);
+bool IsOp(string mystring);
 
-        if(IsOp(prt->data) && !IsOp(prt->right_child->data) && !IsOp(prt->left_child->data)){
-        double num;
-        double num1 = atof(ptr->right_child.c_str()->data.c_str());
-        double num2 = atof(ptr->left_child.c_str()->data.c_str());
-        if(op == '+')
-        {
-            num = num1 + num2;
-        }
-        else if (op == '-')
-        {
-            num = num1 - num2;
-        }
-        else if(op == '/')
-        {
-            num = num1 / num2;
-        }
-        else if (op == '*')
-        {
-            num = num1 * num2;
-        }
-
-        stringstream ss;
-        string result;
-        ss << num;
-        ss >> result;
-
-        cout<<"Resultado = "<< result <<endl;
-
-    }
 };
 
 
--- src/evaluate1.cpp
+++ src/evaluate1.cpp
@@ -7,27 +7,37 @@
 #include "evaluate.hpp"
 #include <string>
 #include <iostream>
+using namespace std;
+
+bool IsOp(string mystring)
+{
+    if(mystring == "-" || mystring == "+" || mystring == "/" || mystring == "*" || mystring == "^")
+        return(true);
+    else
+        return(false);
+}
 
 void evaluate::evaluate(node_type* prt)
-    {
+{
 
-        if(IsOp(prt->data) && !IsOp(prt->right_child->data) && !IsOp(prt->left_child->data)){
+    if(IsOp(prt->data) && !IsOp(prt->right_child->data) && !IsOp(prt->left_child->data))
+    {
         double num;
         double num1 = atof(ptr->right_child.c_str()->data.c_str());
         double num2 = atof(ptr->left_child.c_str()->data.c_str());
-        if(op == '+')
+        if(prt->data == '+')
         {
             num = num1 + num2;
         }
-        else if (op == '-')
+        else if (prt->data == '-')
         {
             num = num1 - num2;
         }
-        else if(op == '/')
+        else if(prt->data == '/')
         {
             num = num1 / num2;
         }
-        else if (op == '*')
+        else if (prt->data == '*')
         {
             num = num1 * num2;
         }
@@ -36,7 +46,19 @@ void evaluate::evaluate(node_type* prt)
         string result;
         ss << num;
         ss >> result;
+        prt->data = result;
+        prt->left_child = NULL;
+        prt->right_child = NULL;
 
         cout<<"Resultado = "<< result <<endl;
 
     }
+    else if(prt->left_child == NULL && prt->right_child == NULL);
+    else
+    {
+        evaluate(prt->left_child);
+        evaluate(prt->right_child);
+        evaluate(prt);
+    }
+
+
